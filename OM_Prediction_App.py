82851ea7e1881f3fa4bafdb63d7cc3c65ae96452
FloydNichols97 <55906620+FloydNichols97@users.noreply.github.com>
2023-11-15 09:40:04 -0600

    Add files via upload

--- OM_Prediction_App.py
+++ OM_Prediction_App.py
@@ -170,12 +170,9 @@ with tab3:
          new_data = pd.read_csv(uploaded_file, nrows=nrows)
          return new_data
 
-    # Create a text element and let the reader know the data is loading.
-    data_load_state = st.text('Loading data...')
-    # Load 10,000 rows of data into the dataframe.
-    new_data = load_data(10000)
-    # Notify the reader that the data was successfully loaded.
-    data_load_state.text("Done!")
+        data_load_state = st.text('Loading data...')
+        new_data = load_data(10000)
+        data_load_state.text("Done!")
 
     if st.checkbox('Show New Data'):
         st.subheader('New Data')
